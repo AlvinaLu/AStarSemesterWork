372b0d5ab894c03e947f521d00402e6026d1bd25
Lushnikova, Alvina <lushnalv@fel.cvut.cz>
2022-08-24 15:34:47 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/Entity.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/Entity.cpp.o"
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/Grid.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/Grid.cpp.o"
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/Mapa.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/Mapa.cpp.o"
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/Priority.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/Priority.cpp.o"
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/Result.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/Result.cpp.o"
+  "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/main.cpp" "/home/alvina/Documents/C_plus/semak/semestalni-prace-pjc/cmake-build-debug/CMakeFiles/semestalni_prace_pjc.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/semestalni_prace_pjc.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/semestalni_prace_pjc.dir/Entity.cpp.o"
+  "CMakeFiles/semestalni_prace_pjc.dir/Grid.cpp.o"
+  "CMakeFiles/semestalni_prace_pjc.dir/Mapa.cpp.o"
+  "CMakeFiles/semestalni_prace_pjc.dir/Priority.cpp.o"
+  "CMakeFiles/semestalni_prace_pjc.dir/Result.cpp.o"
+  "CMakeFiles/semestalni_prace_pjc.dir/main.cpp.o"
+  "semestalni_prace_pjc"
+  "semestalni_prace_pjc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/semestalni_prace_pjc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
